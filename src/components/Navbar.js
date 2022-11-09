51ce12f0d8c0075686d4397a7f634bda122d16d1
Adam S <72041542+AdamS260@users.noreply.github.com>
2022-11-08 22:59:59 -0500

    Improved styling on nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {Link} from 'react-router-dom';
+import './Navbar.css'
 
 const CSCENavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#">CSCE</Navbar.Brand>
+        <Navbar.Brand href="#" id='navBrand'>CSCE</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/events">Events</Link>
-            <Link to="/mentorship">Mentorship</Link>
+          <Nav className="me-auto" id="Navbar">
+            <Link to="/" className="navLink">Home</Link>
+            <Link to="/about" className="navLink">About</Link>
+            <Link to="/contact" className="navLink">Contact</Link>
+            <Link to="/events" className="navLink">Events</Link>
+            <Link to="/mentorship" className="navLink">Mentorship</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
