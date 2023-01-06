3f56f5e1faac420d1a859a107e68fca493cd6f2c
Adam S <72041542+AdamS260@users.noreply.github.com>
2023-01-06 15:04:45 -0500

    Modified Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,56 @@
 /* eslint-disable prettier/prettier */
 import EventCard from '../components/EventCard';
+import Events from '../data/Events.json';
 import './Home.css';
 
 const Home = () => {
-  const testEvent = {
-    eventName: 'test',
-    eventDate: 'December 25, 2022',
-    eventDescription:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    eventImgSrc:
-      'https://static01.nyt.com/images/2011/01/14/arts/14MOVING-span/MOVING-jumbo.jpg',
-    eventRegisterLink: '#',
-    eventTypes: ['Information Session', 'Panel'],
-    eventLocation: 'Benson Building Room 300',
-    eventAddress: '80 Huron Street',
-  };
-  const testEvent2 = {
-    eventName: 'test',
-    eventDate: 'December 25, 2022',
-    eventDescription:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    eventImgSrc:
-      'https://static01.nyt.com/images/2011/01/14/arts/14MOVING-span/MOVING-jumbo.jpg',
-    eventRegisterLink: 'https://www.google.com/',
-    eventTypes: ['Career Fair', 'Fundraiser'],
-    eventLocation: 'Benson Building Room 300',
-    eventAddress: '80 Huron Street',
-  };
-  const testEvent3 = {
-    eventName: 'test',
-    eventDate: 'December 25, 2022',
-    eventDescription:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    eventImgSrc:
-      'https://static01.nyt.com/images/2011/01/14/arts/14MOVING-span/MOVING-jumbo.jpg',
-    eventRegisterLink: 'https://www.google.com/',
-    eventTypes: ['Fundraiser', 'Food Provided'],
-    eventLocation: 'Benson Building Room 300',
-    eventAddress: '80 Huron Street',
-  };
+  let UpcomingEventCardList = [];
+
+  //returns all event objects with a starting date past the current date
+  const filterUpcomingEvents = (eventData) => {
+    //array of upcoming events
+    let upcomingEvents = []
+    const currentDate = new Date()
+    for (let i = 0; i < eventData.length; i++) {
+      // convert the data stored in the json file to a javascript date using a date string
+      const dateOfEvent = new Date(eventData[i].eventDate + " " + eventData[i].eventStartTime)
+      //if the event starting date is past the current date, add it to the array of upcoming events
+      if (dateOfEvent > currentDate) {
+        upcomingEvents.push(eventData[i])
+      }
+    }
+    // return all upcoming events 
+    return upcomingEvents;
+  }
+
+  // creates a list of EventCards given a list of event objects
+  const createEventCards = (eventData) => {
+    // create event cards using data
+    let eventCardList = [];
+    for (let i = 0; i < eventData.length; i++) {
+      eventCardList.push(
+        <div className={(i == eventData.length - 1) ? "CardHolder BottomCardHolder" : "CardHolder"}>
+          <EventCard
+            eventName={eventData[i].eventName}
+            eventDate={eventData[i].eventDate}
+            eventStartTime={eventData[i].eventStartTime}
+            eventEndTime={eventData[i].eventEndTime}
+            eventDescription={eventData[i].eventDescription}
+            eventRegisterLink={eventData[i].eventRegisterLink}
+            eventTypes={eventData[i].eventTypes}
+            eventLocation={eventData[i].eventLocation}
+            eventAddress={eventData[i].eventAddress}
+          />
+        </div>
+      );
+    }
+    //return list of cards
+    return eventCardList;
+  }
+
+  const currentEvents = filterUpcomingEvents(Events);
+  UpcomingEventCardList = createEventCards(currentEvents);
+  
   return (
     <div>
       <div>
@@ -60,42 +72,7 @@ const Home = () => {
             Check out some of our upcoming events
           </p>
         </div>
-        <div className="CardHolder">
-          <EventCard
-            eventName={testEvent.eventName}
-            eventDate={testEvent.eventDate}
-            eventDescription={testEvent.eventDescription}
-            eventImgSrc={testEvent.eventImgSrc}
-            eventRegisterLink={testEvent.eventRegisterLink}
-            eventTypes={testEvent.eventTypes}
-            eventLocation={testEvent.eventLocation}
-            eventAddress={testEvent.eventAddress}
-          />
-        </div>
-        <div className="CardHolder">
-          <EventCard
-            eventName={testEvent2.eventName}
-            eventDate={testEvent2.eventDate}
-            eventDescription={testEvent2.eventDescription}
-            eventImgSrc={testEvent2.eventImgSrc}
-            eventRegisterLink={testEvent2.eventRegisterLink}
-            eventTypes={testEvent2.eventTypes}
-            eventLocation={testEvent2.eventLocation}
-            eventAddress={testEvent2.eventAddress}
-          />
-        </div>
-        <div className="CardHolder BottomCardHolder">
-          <EventCard
-            eventName={testEvent3.eventName}
-            eventDate={testEvent3.eventDate}
-            eventDescription={testEvent3.eventDescription}
-            eventImgSrc={testEvent3.eventImgSrc}
-            eventRegisterLink={testEvent3.eventRegisterLink}
-            eventTypes={testEvent3.eventTypes}
-            eventLocation={testEvent3.eventLocation}
-            eventAddress={testEvent3.eventAddress}
-          />
-        </div>
+        {UpcomingEventCardList}
       </div>
     </div>
   );
