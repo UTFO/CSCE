da92613cf611c0492df4202fa678a48ea0f5dd67
ozzy <o.sultan2811@gmail.com>
2023-01-08 14:05:15 -0500

    react fonts and font awesome should work now

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,19 +1,10 @@
-import {
-  brands,
-  icon,
-  regular,
-  solid,
-} from '@fortawesome/fontawesome-svg-core/import.macro'; // <-- import styles to be used
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaBeer } from 'react-icons/fa';
 
 const HeroSection = () => {
   return (
-    <main>
-      <FontAwesomeIcon icon={solid('user-secret')} />
-      <FontAwesomeIcon icon={regular('coffee')} />
-      <FontAwesomeIcon icon={icon({ name: 'coffee', style: 'solid' })} />
-      <FontAwesomeIcon icon={brands('twitter')} />
-    </main>
+    <h3>
+      Lets go for a <FaBeer />?
+    </h3>
   );
 };
 
