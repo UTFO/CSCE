06294eccc4d7c244f3b189acf05be87e67388460
Houman Ebrahimi <90071124+HoumanEbrahimi@users.noreply.github.com>
2023-01-13 14:17:25 -0500

    Add files via upload

---
+++ src/pages/Mentorship.jsx
@@ -0,0 +1,37 @@
+import "../components/About.css";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { FaInstagram ,FaFacebook} from "react-icons/fa";
+
+
+function Mentorship (){
+    return (
+
+        <div className="mentor_background" >
+
+            <br/>
+            <h1 className="titles">Get in touch with us </h1>
+            <p className="iframe_adjust">           
+                 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfI-9iRW-o_zewKhAqz5yD1UA9vp7w98QZu00H8jUE_RTN77w/viewform?embedded=true" width="640" height="948" frameborder="10" marginheight="0" marginwidth="10">Loading…</iframe>   
+            </p>
+            
+
+            <h2 className="center" >
+                Follow us on social media 
+                <br/>
+            </h2>
+
+            <div className="center"> 
+                <a href="https://www.instagram.com/csce.uoft/?hl=en">
+                    <FaInstagram size={50}/> 
+                </a >
+
+                <a href="https://www.facebook.com/CSCEUofT/">
+                    <FaFacebook size={47}/>    
+                </a>
+            </div>
+
+        </div>
+    );
+}
+
+export default Mentorship;
