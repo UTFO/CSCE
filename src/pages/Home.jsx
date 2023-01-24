6e07a66feea636635a62ff7cfa1db4428f5a79c6
ozzy <o.sultan2811@gmail.com>
2023-01-23 21:19:43 -0500

    hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable prettier/prettier */
+import '../App.css';
 import EventCard from '../components/EventCard';
 import Events from '../data/Events.json';
 import './Home.css';
-import '../App.css';
 
 const Home = () => {
   let UpcomingEventCardList = [];
@@ -10,34 +10,35 @@ const Home = () => {
   //returns all event objects with a starting date past the current date
   const filterUpcomingEvents = (eventData) => {
     //array of upcoming events
-    let upcomingEvents = []
-    const currentDate = new Date()
+    let upcomingEvents = [];
+    const currentDate = new Date();
     for (let i = 0; i < eventData.length; i++) {
       //variable to store time as 24 h
       let startTime;
       //convert event start time to 24h format
-      if (eventData[i].eventStartTime.includes("a.m.")) {
-        const index = eventData[i].eventStartTime.indexOf(" ")
-        startTime = eventData[i].eventStartTime.substring(0,index)
-      }
-      else if (eventData[i].eventStartTime.includes("p.m.")) {
+      if (eventData[i].eventStartTime.includes('a.m.')) {
+        const index = eventData[i].eventStartTime.indexOf(' ');
+        startTime = eventData[i].eventStartTime.substring(0, index);
+      } else if (eventData[i].eventStartTime.includes('p.m.')) {
         //convert hour into 24 h format
-        const hour = 12 + parseInt(eventData[i].eventStartTime.substring(0,2))
+        const hour = 12 + parseInt(eventData[i].eventStartTime.substring(0, 2));
         //add converted hour and minutes to full string
         //i.e "13" + ":30" -> "13:30"
-        const indexOfColon = eventData[i].eventStartTime.indexOf(":")
-        startTime = String(hour) + eventData[i].eventStartTime.substring(indexOfColon, indexOfColon + 2)
+        const indexOfColon = eventData[i].eventStartTime.indexOf(':');
+        startTime =
+          String(hour) +
+          eventData[i].eventStartTime.substring(indexOfColon, indexOfColon + 2);
       }
       // convert the data stored in the json file to a javascript date using a date string
-      const dateOfEvent = new Date(eventData[i].eventDate + " " + startTime)
+      const dateOfEvent = new Date(eventData[i].eventDate + ' ' + startTime);
       //if the event starting date is past the current date, add it to the array of upcoming events
       if (dateOfEvent > currentDate) {
-        upcomingEvents.push(eventData[i])
+        upcomingEvents.push(eventData[i]);
       }
     }
-    // return all upcoming events 
+    // return all upcoming events
     return upcomingEvents;
-  }
+  };
 
   // creates a list of EventCards given a list of event objects
   const createEventCards = (eventData) => {
@@ -45,7 +46,13 @@ const Home = () => {
     let eventCardList = [];
     for (let i = 0; i < eventData.length; i++) {
       eventCardList.push(
-        <div className={(i == eventData.length - 1) ? "CardHolder BottomCardHolder" : "CardHolder"}>
+        <div
+          className={
+            i == eventData.length - 1
+              ? 'CardHolder BottomCardHolder'
+              : 'CardHolder'
+          }
+        >
           <EventCard
             eventName={eventData[i].eventName}
             eventDate={eventData[i].eventDate}
@@ -62,16 +69,50 @@ const Home = () => {
     }
     //return list of cards
     return eventCardList;
-  }
+  };
 
   const currentEvents = filterUpcomingEvents(Events);
   UpcomingEventCardList = createEventCards(currentEvents);
-  
+
   return (
     <div>
-      <div>
-        <h1>This is the home page</h1>
+      <div className="container col-xxl-8 px-4 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div className="col-10 col-sm-8 col-lg-6">
+            <img
+              src="../assets/heropicture.svg"
+              className="d-block mx-lg-auto img-fluid"
+              alt="Bootstrap Themes"
+              width="700"
+              height="500"
+              loading="lazy"
+            />
+          </div>
+          <div className="col-lg-6">
+            <h1 className="display-5 fw-bold lh-1 mb-3">
+              Canadian Society of Civil Engineers
+            </h1>
+            <p className="lead">University of Toronto Chapter</p>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+              <button
+                type="button"
+                className="btn btn-primary btn-lg px-4 me-md-2"
+              >
+                Primary
+              </button>
+              <button
+                type="button"
+                className="btn btn-outline-secondary btn-lg px-4"
+              >
+                Default
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <div className="spacer wavylayer"></div>
+
       <div className="EventHolder enlarge">
         <div style={{ textAlign: 'center', paddingTop: 20 }}>
           <h2 className="EventHolderTitle">Upcoming Events</h2>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import macrosPlugin from 'vite-plugin-babel-macros';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), macrosPlugin()],
+  plugins: [react()],
 });
