1bb055388c34eb605dddd7bb44517af082df77d1
ozzy <o.sultan2811@gmail.com>
2023-01-24 22:53:02 -0500

    fixed images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
-/* eslint-disable prettier/prettier */
 import '../App.css';
+import LandscapeSvg from '../assets/landscape.svg';
 import EventCard from '../components/EventCard';
 import Events from '../data/Events.json';
 import './Home.css';
-import '../App.css';
-import LandscapeSvg from '../assets/landscape.svg'
 
 const Home = () => {
   let UpcomingEventCardList = [];
@@ -78,11 +76,40 @@ const Home = () => {
 
   return (
     <div className="background">
-      <div>
-        <h1>This is the home page</h1>
+      <div className="container col-xxl-8 px-4 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div className="col-10 col-sm-8 col-lg-6">
+            <img
+              src="../assets/heropicture.svg"
+              className="d-block mx-lg-auto img-fluid"
+              alt="Bootstrap Themes"
+              width="700"
+              height="500"
+              loading="lazy"
+            />
+          </div>
+          <div className="col-lg-6">
+            <h1 className="display-5 fw-bold lh-1 mb-3">
+              Canadian Society of Civil Engineers
+            </h1>
+            <p className="lead">University of Toronto Chapter</p>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+              <button
+                type="button"
+                className="btn btn-primary btn-lg px-4 me-md-2"
+              >
+                Primary
+              </button>
+              <button
+                type="button"
+                className="btn btn-outline-secondary btn-lg px-4"
+              >
+                Default
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="spacer wavylayer"></div>
 
       <div className="EventHolder enlarge">
         <div style={{ textAlign: 'center', paddingTop: 20 }}>
@@ -103,7 +130,7 @@ const Home = () => {
         {UpcomingEventCardList}
       </div>
       <div>
-        <img src={LandscapeSvg} alt='Landscape'/>
+        <img src={LandscapeSvg} alt="Landscape" />
       </div>
     </div>
   );
