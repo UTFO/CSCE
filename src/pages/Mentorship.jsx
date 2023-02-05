da7d6004a13f1404fb45df1e9392fb3301a039b7
Houman Ebrahimi <90071124+HoumanEbrahimi@users.noreply.github.com>
2023-02-05 17:50:13 -0500

    Updated item shadows and icon colors

--- src/pages/Mentorship.jsx
+++ src/pages/Mentorship.jsx
@@ -1,38 +1,42 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { FaFacebook, FaInstagram } from 'react-icons/fa';
-import '../components/Mentor.css';
+import "../components/Mentor.css";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { FaInstagram ,FaFacebook} from "react-icons/fa";
 
-function Mentorship() {
-  return (
-    <div className="mentor_background">
-      <br />
-      <h1 className="titles">Get in touch with us </h1>
-      <p className="iframe_adjust">
-        <iframe
-          src="https://docs.google.com/forms/d/e/1FAIpQLSfI-9iRW-o_zewKhAqz5yD1UA9vp7w98QZu00H8jUE_RTN77w/viewform?embedded=true"
-          width="640"
-          height="948"
-          frameBorder="10"
-          marginHeight="0"
-          marginWidth="10"
-        >
-          Loading…
-        </iframe>
-      </p>
 
-      <h2 className="center">
-        Follow us on social media
-        <br />
-      </h2>
+function Mentorship (){
+    return (
 
-      <div className="center">
-        <a href="https://www.instagram.com/csce.uoft/?hl=en">
-          <FaInstagram size={50} />
-        </a>
+        <div className="mentor_background" >
 
-        <a href="https://www.facebook.com/CSCEUofT/">
-          <FaFacebook size={47} />
-        </a>
+            <br/>
+            <h1 className="titles">Get in touch with us </h1>
+            <p className="center">           
+                 <iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSfI-9iRW-o_zewKhAqz5yD1UA9vp7w98QZu00H8jUE_RTN77w/viewform?embedded=true" width="640" height="948" frameborder="10" marginheight="0" marginwidth="10">Loading…</iframe>   
+            </p>
+            
+
+            <h2 className="center" >
+                Follow us on social media 
+                <br/>
+            </h2>
+
+            <div className="center"> 
+            <p>
+                <a href="https://www.instagram.com/csce.uoft/?hl=en">
+                    <FaInstagram class="icon" size={50}/> 
+                </a >
+
+                <a href="https://www.facebook.com/CSCEUofT/">
+                    <FaFacebook class="icon" size={47}/>    
+                </a>
+                </p>
+            </div>
+
+        </div>
+    );
+}
+
+export default Mentorship;
       </div>
     </div>
   );
