8bc5de2d4c71dc79c13183e09782cd04b8cd4b20
Houman Ebrahimi <90071124+HoumanEbrahimi@users.noreply.github.com>
2023-03-20 23:42:07 -0400

    updated the flip card mechanism
    Once the user hovers over an icon, the card flips. The description at the back of each includes the "about me" section and a link to the users linkedin profile

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,109 +1,137 @@
-import Users from '../Data/Users.json';
-import './About.css';
 
-function About() {
-  const filterExec = Users.filter((users) => users.id.includes('executive'));
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Row from "react-bootstrap/Row";
+import Col from 'react-bootstrap/Col';
+import { Container } from 'react-bootstrap';
+import Users from '../Data/Users.json'
+import "../components/About.css";
+import { FaInstagram, FaUser } from 'react-icons/fa';
+import { FaLinkedin} from 'react-icons/fa';
 
-  const filterAso = Users.filter((users) => users.id.includes('associate'));
+import ReactCardFlip from "react-card-flip";
 
-  console.log(filterExec, filterAso);
-  return (
-    <div className="background">
-      <section id="about" className="px-5 py-2">
-        <div className="container">
-          <h3 className="text-center">About CSCE UofT</h3>
-          <p>
-            We are a networking club for civil engineering students. We help
-            students discover different career paths by connecting students to
-            mentors in the industry, hosting panels about different fields in
-            civil engineering, hold skills workshops, and help run career fair.
-          </p>
-        </div>
+import React, { useState } from 'react';
+
+function About(){
+    const [flipped, setFlip] = useState(false);
+
+    const filterExec=Users.filter(users=>users.id.includes("executive"));
+    
+  
+   const filterAso=Users.filter(users=>users.id.includes("associate"));
+  
+  
+    const test = (id,aux)=>{
+      let lists=[]
+      for(let i=0;i<aux.length;i++) {
+        if(aux[i].index==id){
+          aux[id].flip=!flipped[id];
+  
+        }
+        else{
+          aux[i].flip=false;
+        }
+      }
+      for(let i=0;i<aux.length;i++) {
+        lists.push(aux[i].flip);
+      }
+      setFlip(lists);
+      console.log(aux)
+    }
+    var cards = document.querySelectorAll('.card');
+
+    return(
+      
+        <div className="background">
+
+        <section id="about" className="px-5 py-2">
+        <h1 className="titles">
+          About us 
+        </h1>
+
+        <h3 className="titles"> 
+        We are a networking club for civil engineering students. 
+        We help students discover different career paths by connecting students to mentors in the industry,
+         hosting panels about different fields in civil engineering, hold skills workshops, and help run career fair
+        </h3>
       </section>
 
-      <h3 className="text-center">Meet the Team!</h3>
+      <form class="title_form"> 
+      <h2 className="accent left accent-left"> &nbsp;&nbsp;Executives</h2>
+      </form>
+      <label className="card-container" htmlFor="card-2">
 
-      <div className="title_form">
-        <br></br>
-
-        <form className="membertype">
-          <h2 className="accent left accent-left"> Executives</h2>
-        </form>
-        <br></br>
-        <input type="checkbox" id="card-2" />
-        <label className="card-container" htmlFor="card-2">
-          <div className="row row-cols-5 px-5 mx-5 px-5">
-            {filterExec.map((user) => (
+        <div className="row row-cols-5 px-5 mx-5 px-5 ">
+              {filterExec.map((user) => (
               <div className="col-12 col-md-6 col-lg-4 ">
-                <div className="section">
-                  <div className="card-flip">
-                    <div className="card front" style={{ width: 270 }}>
-                      <img
-                        src={user.photo}
-                        alt={user.name}
-                        style={{ width: 270, height: 300 }}
-                        className="card-img-top"
-                      />
 
-                      <div className="card-block">
-                        <h4 className="card-title text-center">{user.name}</h4>
-                        <h6 className="card-subtitle mb-2 text-muted text-center">
-                          {user.title}
-                        </h6>
-                      </div>
+            <div class="flip-card">
+            <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <img src={user.photo} alt="Avatar" style={{ width: 270, height: 300 }}/>
+                <div className="card-block">
+                    <h4 className="card-title text-center">{user.name}</h4>
+                    <h6 className="card-subtitle mb-2 text-muted text-center">
+                        {user.title}
+                    </h6>
                     </div>
 
-                    <div className="card back" style={{ width: 300 }}>
-                      <p className="card-text">{user.about}.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
+            </div>
+        <div class="flip-card-back">
+        <h2> About me </h2>
+
+            <p>{user.about}</p> 
+            <a href="https://www.linkedin.com/feed/" >  <FaLinkedin class="icon" size={47} />
+</a>
+
           </div>
-        </label>
-
-        <br></br>
-
-        <form className="membertype">
-          <h2 className="accent left accent-left"> Associates</h2>
-        </form>
-        <br></br>
-        <input type="checkbox" id="card-1" />
-        <label className="card-container" htmlFor="card-1">
-          <div className="row row-cols-5 px-5 mx-5 px-5">
-            {filterAso.map((user) => (
-              <div className="col-12 col-md-6 col-lg-4 ">
-                <div className="section">
-                  <div className="card-flip">
-                    <div className="card front" style={{ width: 270 }}>
-                      <img
-                        src={user.photo}
-                        alt={user.name}
-                        style={{ width: 270, height: 300 }}
-                        className="card-img-top"
-                      />
-
-                      <div className="card-block">
-                        <h4 className="card-title text-center">{user.name}</h4>
-                        <h6 className="card-subtitle mb-2 text-muted text-center">
-                          {user.title}
-                        </h6>
-                      </div>
-                    </div>
-
-                    <div className="card back" style={{ width: 300 }}>
-                      <p className="card-text">{user.about}.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </label>
+        </div>
       </div>
+      </div>
+
+))}
+
     </div>
-  );
+    </label>
+    <form class="title_form"> 
+      <h2 className="accent left accent-left"> &nbsp;&nbsp;Associates</h2>
+      </form>
+
+      <div className="row row-cols-5 px-5 mx-5 px-5">
+              {filterAso.map((user) => (
+              <div className="col-12 col-md-6 col-lg-4 ">
+
+            <div class="flip-card">
+            <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <img src={user.photo} alt="Avatar" style={{ width: 270, height: 300 }}/>
+                <div className="card-block">
+                    <h4 className="card-title text-center">{user.name}</h4>
+                    <h6 className="card-subtitle mb-2 text-muted text-center">
+                        {user.title}
+                    </h6>
+                    </div>
+            </div>
+        <div class="flip-card-back">
+            <h2> About me </h2>
+            <p>{user.about}</p> 
+          </div>
+        </div>
+      </div>
+      </div>
+
+))}
+
+    </div>
+      
+    </div>
+      
+    )
+
+
+
 }
 
 export default About;
